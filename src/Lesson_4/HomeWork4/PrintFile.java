44b4954d56a963147809fa4130118786a9533b0d
petrol95 <petrol195@yandex.ru>
2019-01-09 16:34:11 +0300

    deleted extra imports

--- src/Lesson_4/HomeWork4/PrintFile.java
+++ src/Lesson_4/HomeWork4/PrintFile.java
@@ -3,9 +3,6 @@ package Lesson_4.HomeWork4;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 
 public class PrintFile {
 
