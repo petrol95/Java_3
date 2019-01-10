82d13bba0582e76d7e29dd51d415d70497c311cb
petrol95 <petrol195@yandex.ru>
2019-01-10 16:59:04 +0300

    fixed threads errors

--- src/Lesson_5/Races/Car.java
+++ src/Lesson_5/Races/Car.java
@@ -1,7 +1,12 @@
 package Lesson_5.Races;
 
-public class Car implements Runnable {
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
+
+//public class Car implements Runnable {
+public class Car {
     private static int CARS_COUNT;
+
     static {
         CARS_COUNT = 0 ;
     }
@@ -24,18 +29,39 @@ public class Car implements Runnable {
         this.name = "Участник #" + CARS_COUNT;
     }
 
-    @Override
-    public void run() {
+    public void runCar(CyclicBarrier cb) {
         try {
-            System.out.println(this .name + " готовится");
-            Thread.sleep(500 + (int)(Math.random() * 800));
-            System.out.println(this .name + " готов");
+            System.out.println(this.name + " готовится");
+            Thread.sleep(500 + (int) (Math.random() * 800));
+            System.out.println(this.name + " готов");
+            cb.await();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        for (int i = 0; i < race.getStages().size(); i++) {
-            race.getStages().get(i).go(this);
-        }
     }
-}
 
+    public void goTunnel(Semaphore smp, Tunnel tunnel) {
+        try {
+            try {
+                System.out.println(this.name + " готовится к этапу(ждет): " + tunnel.description);
+                smp.acquire();
+                System.out.println(this.name + " начал этап: " + tunnel.description);
+                Thread.sleep(tunnel.length / this.getSpeed() * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println(this.name + " закончил этап: " + tunnel.description);
+                smp.release();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+//        for (int i = 0; i < race.getStages().size(); i++) {
+//            if (!race.getStages().get(i).getClass().getName().equals("Lesson_5.Races.Tunnel"))
+//                race.getStages().get(i).go(this);
+//        }
+
+    }
+
+}
--- src/Lesson_5/Races/MainClass.java
+++ src/Lesson_5/Races/MainClass.java
@@ -1,19 +1,71 @@
 package Lesson_5.Races;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
+
 public class MainClass {
     public static final int CARS_COUNT = 4 ;
 
     public static void main(String[] args) {
+
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
-        Race race = new Race( new Road(60), new Tunnel(), new Road(40));
+
+        Tunnel tunnel = new Tunnel();
+        Race race = new Race( new Road(60), tunnel, new Road(40));
         Car[] cars = new Car[CARS_COUNT];
-        for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int)(Math.random() * 10));
+
+        for (int j = 0; j < cars.length; j++) {
+            cars[j] = new Car(race, 20 + (int)(Math.random() * 10));
         }
-        for (int i = 0; i < cars.length; i++) {
-            new Thread(cars[i]).start();
+
+
+
+//        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+
+
+
+        final CountDownLatch cdl = new CountDownLatch(CARS_COUNT);
+        for (int i = 0; i < CARS_COUNT; i++) {
+//            final int w = i;
+            new Thread(() -> {
+                try {
+
+                    CyclicBarrier cb = new CyclicBarrier(CARS_COUNT);
+
+                    for (int j = 0; j < cars.length; j++) {
+                        final int w = j;
+                        new Thread(() -> {
+                            cars[w].runCar(cb);
+                        }).start();
+
+                    }
+
+
+
+
+//                    // Tunnel
+//                    Semaphore smp = new Semaphore(CARS_COUNT / 2);
+//                    for (int j = 0; j < cars.length; j++) {
+//                        final int v = j;
+//                        new Thread(() -> {
+//                            cars[v].goTunnel(smp, tunnel);
+//                        }).start();
+//
+//                    }
+
+                    Thread.sleep( 500 + ( int )( 500 * Math.random()));
+                    cdl.countDown();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }).start();
         }
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        try {
+            cdl.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
